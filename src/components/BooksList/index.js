fcba2aa5dda94206b87843d371c9da9b8a621c1b
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-05-31 21:24:08 +0200

    Add home page with sections

---
+++ src/components/BooksList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+function BooksList() {
+  return(
+    <div className='BooksList'>
+      books list
+    </div>
+  );
+}
+
+export default BooksList;
---
+++ src/components/Form/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+function Form() {
+  return (
+    <div className='Form'>
+      form
+    </div>
+  );
+}
+
+export default Form;
