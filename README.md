2894cc7db3ba27bbe7f0dd78da115b4924d0fcc7
Kamil Mastalerz <kamil.mastalerz7@wp.pl>
2019-06-01 00:04:44 +0200

    Fix README and add post() implementation

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 App was created to add books which you read and save it into Table.
 
+## How to run
+
+1. Use `npm install` in root to install all libraries
+2. Use `npm start` to turn on frontend
+3. Go to `server` catalog to turn on server app
+4. Use `node index.js` to turn on server app
+
 ## Libraries and Frameworks used used
 
 ### Frontend 
---
+++ src/api/utils.js
@@ -0,0 +1,12 @@
+const apiEndpoint = 'http://localhost:4000' // TODO move it to process.env.API_ENDPOINT variable
+
+export function post(url, body) {
+  return fetch(apiEndpoint + url, {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(body),
+  });
+}
